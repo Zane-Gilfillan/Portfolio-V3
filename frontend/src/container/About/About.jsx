b3012653b5fb273157b527f1aff15da8264aa40c
Zane-Gilfillan <zane.gilfillan@gmail.com>
2022-06-24 14:55:27 -0500

    working through mapping about section

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import { motion } from 'framer-motion';
 import './About.scss';
+
+const abouts = [
+  { title: 'web dev', description: 'lorem ipsum dolor sit', imgUrl: ''},
+  { title: 'ux/ui', description: 'lorem ipsum dolor sit', imgUrl: ''},
+  { title: 'audio design', description: 'lorem ipsum dolor sit', imgUrl: ''}
+]
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <h2 className="head-text">
+        Lorem, ipsum dolor.
+        <span>Lorem, ipsum.</span>
+        <br />
+        Lorem.
+        <span>Lorem ipsum dolor sit.</span>
+       </h2>
+
+       <div className="app__profiles">
+        {abouts.map((abouts, index) => (
+          <motion.div>
+            
+          </motion.div>
+        ))}
+       </div>
+    </>
   )
 }
 
