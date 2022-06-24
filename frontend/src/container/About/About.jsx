c522db1d0d62c7c9c1497124ebfffcfe536b1294
Zane-Gilfillan <zane.gilfillan@gmail.com>
2022-06-24 16:18:31 -0500

    finished about section

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,28 +1,31 @@
 import React, {useState, useEffect} from 'react';
 import { motion } from 'framer-motion';
 import './About.scss';
+import { images } from '../../constants'
 
 const abouts = [
-  { title: 'web dev', description: 'lorem ipsum dolor sit', imgUrl: ''},
-  { title: 'ux/ui', description: 'lorem ipsum dolor sit', imgUrl: ''},
-  { title: 'audio design', description: 'lorem ipsum dolor sit', imgUrl: ''}
+  { title: 'web dev', description: 'lorem ipsum dolor sit', imgUrl: images.about01},
+  { title: 'ux/ui', description: 'lorem ipsum dolor sit', imgUrl: images.about02},
+  { title: 'audio design', description: 'lorem ipsum dolor sit', imgUrl: images.about04}
 ]
 
 const About = () => {
   return (
     <>
-      <h2 className="head-text">
-        Lorem, ipsum dolor.
-        <span>Lorem, ipsum.</span>
-        <br />
-        Lorem.
-        <span>Lorem ipsum dolor sit.</span>
-       </h2>
+      <h2 className="head-text">Lorem, ipsum dolor. <span> Lorem, ipsum. </span> <br /> Lorem. <span> Lorem ipsum dolor sit. </span></h2>
 
        <div className="app__profiles">
-        {abouts.map((abouts, index) => (
-          <motion.div>
-            
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween'}}
+            className='app__profile-item'
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
         ))}
        </div>
