45d70e68e1794d2506d52eab01f5489377445555
Zane-Gilfillan <zane.gilfillan@gmail.com>
2022-06-24 16:53:02 -0500

    connected sanity to frontend

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -2,14 +2,20 @@ import React, {useState, useEffect} from 'react';
 import { motion } from 'framer-motion';
 import './About.scss';
 import { images } from '../../constants'
-
-const abouts = [
-  { title: 'web dev', description: 'lorem ipsum dolor sit', imgUrl: images.about01},
-  { title: 'ux/ui', description: 'lorem ipsum dolor sit', imgUrl: images.about02},
-  { title: 'audio design', description: 'lorem ipsum dolor sit', imgUrl: images.about04}
-]
+import { urlFor, client } from '../../client'
 
 const About = () => {
+
+  const [abouts, setAbouts] = useState([])
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query)
+      .then((data) => setAbouts(data))
+  }, []);
+  
+
   return (
     <>
       <h2 className="head-text">Lorem, ipsum dolor. <span> Lorem, ipsum. </span> <br /> Lorem. <span> Lorem ipsum dolor sit. </span></h2>
@@ -23,7 +29,7 @@ const About = () => {
             className='app__profile-item'
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
             <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
