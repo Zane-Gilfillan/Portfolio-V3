fdee02b3f6075d3359a024a073bbef6f01c584e8
Zane-Gilfillan <zane.gilfillan@gmail.com>
2022-06-24 17:25:57 -0500

    adding nav bar on right side

--- frontend/src/wrapper/AppWrap.js
+++ frontend/src/wrapper/AppWrap.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { NavigationDots, SocialMedia } from '../components'
 
-const AppWrap = (Component, id, classNames) => function HOC() {
+const AppWrap = (Component, idName, classNames) => function HOC() {
   return (
-    <div id={idName}>
-
+    <div id={idName} className={`app__container ${classNames}`}>
+        <SocialMedia />
+        <div className="app__wrapper app__flex">
+            <Component />
+            <div className="copyright">
+                <p className="p-text">@2022 zmlm</p>
+                
+            </div>
+        </div>
+        <NavigationDots active={idName} />
     </div>
   )
 }
