12e5853e386e57d4bf658250e9c14a594618e72f
Zane-Gilfillan <zane.gilfillan@gmail.com>
2022-06-28 15:12:24 -0500

    linked up contact form to sanity

--- frontend/src/container/Footer/Footer.jsx
+++ frontend/src/container/Footer/Footer.jsx
@@ -2,78 +2,84 @@ import React, { useState } from 'react';
 import { images } from '../../constants';
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { client } from '../../client';
-import './Footer.scss'
+import './Footer.scss';
 
 const Footer = () => {
-
-  const [formData, setFormData] = useState({ name: '', email: '', message: ''});
+  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const { name, email, message } = formData;
+  const { username, email, message } = formData;
 
   const handleChangeInput = (e) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value })
-
-  }
+    setFormData({ ...formData, [name]: value });
+  };
 
   const handleSubmit = () => {
     setLoading(true);
 
     const contact = {
       _type: 'contact',
-      name: name,
-      email: email,
-      message: message
-    }
+      name: formData.username,
+      email: formData.email,
+      message: formData.message,
+    };
 
     client.create(contact)
-      .them(() => {
+      .then(() => {
         setLoading(false);
         setIsFormSubmitted(true);
       })
-
-  }
+      .catch((err) => console.log(err));
+  };
 
   return (
     <>
-      <h2 className="head-text">let's talk!</h2>
+      <h2 className="head-text">lets talk 📱</h2>
 
       <div className="app__footer-cards">
-        <div className="app__footer-card">
+        <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:zane.gilfillan@gmail.com" className='p-text'>zane.gilfillan@gmail.com</a>
+          <a href="mailto:zane.gilfillan@gmail.com" className="p-text">zane.gilfillan@gmail.com</a>
         </div>
-
         <div className="app__footer-card">
-          <img src={images.mobile} alt="mobile" />
-          <a href="tel: +1 (312) 446-5971" className='p-text'>+1 (312) 446-5971</a>
+          <img src={images.mobile} alt="phone" />
+          <a href="tel:+1 (312) 446-5971" className="p-text">+1 (312) 446-5971</a>
         </div>
       </div>
-    {!isFormSubmitted ?       
-      <div className="app__footer-form app__flex">
-        <div className="app__flex">
-          <input className='p-text' type="text" placeholder='your name' name='name' value={name} onChange={handleChangeInput} />
+      {!isFormSubmitted ? (
+        <div className="app__footer-form app__flex">
+          <div className="app__flex">
+            <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />
+          </div>
+          <div className="app__flex">
+            <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />
+          </div>
+          <div>
+            <textarea
+              className="p-text"
+              placeholder="Your Message"
+              value={message}
+              name="message"
+              onChange={handleChangeInput}
+            />
+          </div>
+          <button type="button" className="p-text" onClick={handleSubmit}>{!loading ? 'Send Message' : 'Sending...'}</button>
         </div>
-        <div className="app__flex">
-          <input className='p-text' type="email" placeholder='your email' name='email' value={email} onChange={handleChangeInput} />
+      ) : (
+        <div>
+          <h3 className="head-text">
+            Thank you for reaching out!
+          </h3>
         </div>
-        <div className=''>
-          <textarea className='p-text' placeholder='your message' value={message} name="message" onChange={handleChangeInput} ></textarea>
-        </div>
-        <button className='p-text' type='button' onClick={handleSubmit}>{loading ? 'sending' : 'send message'}</button>
-      </div> 
-      : <div>
-        <h3 className='head-text'> thanks for reaching out!</h3>
-      </div>
-      }
+      )}
     </>
-  )
-}
+  );
+};
 
 export default AppWrap(
   MotionWrap(Footer, 'app__footer'),
   'contact',
-  'app__whitebg'
-)
+  'app__whitebg',
+);
