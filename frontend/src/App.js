2d84476deb6e29d82d37ff5360162842b954dd7a
Zane-Gilfillan <zane.gilfillan@gmail.com>
2022-06-28 14:17:32 -0500

    added HOC motion wrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ const App = () => {
       <Header />
       <About />
       <Work />
+      <Skills />
     </div>
   );
 };
--- frontend/src/container/Footer/Footer.jsx
+++ frontend/src/container/Footer/Footer.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { images } from '../../constants'
 import './Footer.scss'
 
 const Footer = () => {
